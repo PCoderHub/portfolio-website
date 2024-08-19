66203a6554bd53fe930e01887baa2132b559e120
PCoderHub <poojapbalachandran@gmail.com>
2024-08-19 19:29:54 +0100

    update app.js-2

--- back/app.js
+++ back/app.js
@@ -1,19 +1,17 @@
 require('dotenv').config();
-
 require('./db/conn');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes/route');
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
 app.use('/api', routes);
-app.get('/', (req, res) => {
-    res.json('Hello');
-});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on ${process.env.PORT}`);
